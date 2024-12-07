983b18aff41a9e0cd48edee4e0f3445bbf6c436c
Andie Kardos <58579363+AndrejaKardos@users.noreply.github.com>
2024-12-07 22:01:33 +1100

    Added ColorsController and ColorsService to AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ColorsModule } from './colors/colors.module';
+import { ColorsController } from './colors/colors.controller';
+import { ColorsService } from './colors/colors.service';
 
 @Module({
   imports: [ColorsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ColorsController],
+  providers: [AppService, ColorsService],
 })
 export class AppModule {}
