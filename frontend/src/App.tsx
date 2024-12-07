cfd4e892f7ff9de74a86eed3041572c1531c2f45
Andie Kardos <58579363+AndrejaKardos@users.noreply.github.com>
2024-12-07 23:15:29 +1100

    Added ColorSwatch component to App file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,35 @@
 import { useState } from "react";
 import "./App.css";
 import { ColorSwatch } from "./components/colorSwatch";
+import { Color } from "./types/color";
+import { fetchColorSwatches } from "./apis/colorsApi";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [colorSwatches, setColorSwatches] = useState<Color[]>([]);
+
+  const handleFetchColors = async () => {
+    try {
+      const colors = await fetchColorSwatches();
+      setColorSwatches(colors);
+    } catch (err) {
+      console.log("Failed to fetch color swatches. Please try again.");
+    }
+  };
 
   return (
-    <>
-      <div>
-        <h2>Colour Swatches</h2>
-        <ColorSwatch />
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div>
+      <h2>Color Swatches</h2>
+      <div
+        className="color-swatches-container"
+        style={{ display: "flex", gap: "10px", flexWrap: "wrap" }}
+      >
+        {colorSwatches.map((color, index) => (
+          <ColorSwatch key={index} color={color} />
+        ))}
       </div>
-    </>
+
+      <button onClick={handleFetchColors}>Fetch Colors</button>
+    </div>
   );
 }
 
